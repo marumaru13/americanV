e5ebc3c11089a2d9e48e2c8e45af8c20c3c53b1a
富名腰美樹 <l.fnks33@gmail.com>
2021-12-08 15:31:50 +0900

    　

---
+++ footer.php
@@ -0,0 +1,8 @@
+<footer>
+        <p>Copyright<small>&copy;</small>Shinohara. ALL RIGHT RESERVED.</p>
+    </footer>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="./assets/js/script.js"></script>
+</body>
+
+</html>
