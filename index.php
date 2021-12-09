d07b439f3df1194338f47307cddd3f277bda2e41
mai yamamaru <for.life0311@gmail.com>
2021-12-09 14:10:15 +0900

    wordpressできた

--- index.php
+++ index.php
@@ -1,65 +1,24 @@
 <?php get_header(); ?>
 <main>
-        <div class="section2">
-            <h1>Latest Articles</h1>
-            <div class="contents">
-                <div class="box2">
-                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/post_img_1.png" alt="">
-                    <p class="dates">2018/5/20</p>
-                    <p class="text">おしゃれカフェがありますよ</p>
-                    <div class="btn_box">
-                        <p class="read">READ MORE</p>
-                        <span></span>
+  <h1>Latest Articles</h1>
+  <div class="contents">
+   <?php  
+      if(have_posts()):
+        while(have_posts()):
+          the_post(); ?>
+            <div class="box2">
+                <img src="" alt=""><?php the_post_thumbnail(); ?>
+                <p class="dates"><?php echo get_the_date(); ?></p>
+                <p class="text"><?php the_title(); ?></p>
+                 <div class="btn_box">
+                    <p class="read"><a href="<?php the_permalink(); ?>">READ MORE</a></p>
+                    <span></span>
                     </div>
-                </div>
-                <div class="box2">
-                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/post_img_2.png" alt="">
-                    <p class="dates">2018/5/19</p>
-                    <p class="text">あのネオンはいつ交換するのか！？観覧車の謎に迫る！</p>
-                    <div class="btn_box">
-                        <p class="read">READ MORE</p>
-                        <span></span>
-                    </div>
-                </div>
-                <div class="box2">
-                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/post_img_3.png" alt="">
-                    <p class="dates">2018/5/18</p>
-                    <p class="text">ラソナの社内はこんなのよ</p>
-                    <div class="btn_box">
-                        <p class="read">READ MORE</p>
-                        <span></span>
-                    </div>
-                </div>
             </div>
-            <div class="contents">
-                <div class="box2">
-                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/post_img_4.png" alt="">
-                    <p class="dates">2018/5/20</p>
-                    <p class="text">お隣のアラハはハワイ？</p>
-                    <div class="btn_box">
-                        <p class="read">READ MORE</p>
-                        <span></span>
-                    </div>
-                </div>
-                <div class="box2">
-                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/post_img_5.png" alt="">
-                    <p class="dates">2018/5/19</p>
-                    <p class="text">なぜテント？ラソナの人に聞いてみた</p>
-                    <div class="btn_box">
-                        <p class="read">READ MORE</p>
-                        <span></span>
-                    </div>
-                </div>
-                <div class="box2">
-                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/post_img_6.png" alt="">
-                    <p class="dates">2018/5/18</p>
-                    <p class="text">ベイエリアオシャレすぎる問題</p>
-                    <div class="btn_box">
-                        <p class="read">READ MORE</p>
-                        <span></span>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </main>
-    <?php get_footer(); ?>
+        <?php endwhile;
+      else : ?>
+        <p>表示する記事がありません</p>
+    <?php endif;?>
+  </div>
+</main>
+<?php get_footer(); ?>
