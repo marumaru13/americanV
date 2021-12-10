d2f62fa582b86b3624d8e024c2d8636d72dd6940
mai yamamaru <for.life0311@gmail.com>
2021-12-10 14:10:17 +0900

    ハンバーガーメニュー入れた、まだ完成ではない

--- header.php
+++ header.php
@@ -14,16 +14,25 @@
     <header>
         <div class="section1">
             <div　class="content">
-                <div class="inner">
+                <div class="header_items">
+                <div class="hamburger_rap">
+                 <input type="checkbox" id="hamburger">
+                 <label class="hamburger" for="hamburger">
+                 <span class="pate"></span>
+                 </label>  
+
+                 <div class="inner">
                     <p class="menu">Menu01</p>
                     <p class="menu">Menu02</p>
                     <p class="menu">Menu03</p>
                     <p class="menu">Menu04</p>
+                 </div>
                 </div>
                 <div class="box">
                     <img src="<?php echo get_template_directory_uri(); ?>/assets/img/visual_1_pc.png" alt="" class="img_01">
                     <img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo.png" alt="" class="img_02">
                     <img src="<?php echo get_template_directory_uri(); ?>/assets/img/visual_text_pc.png" alt="" class="img_03">
+                </div>
                 </div>
                 <img src="<?php echo get_template_directory_uri(); ?>/assets/img/visual_1_sp.png" alt="" class="spimg_01">
                 <img src="<?php echo get_template_directory_uri(); ?>/assets/img/visual_text_sp.png" alt="" class="spimg_03">
